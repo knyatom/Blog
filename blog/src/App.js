2265f31fff1b3c51e90ed3892f89b3a4db0b2a75
kny <knyatom@gmail.com>
2021-08-23 01:06:50 +0900

    823
    823

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,14 +1,19 @@
-import { blue } from 'chalk';
+
+import React,{useState} from 'react';
 import './App.css';
 import logo from './logo.svg';
 
 function App() {
+
+  let [글제목,글제목변경]=useState('남자 코드 추천');
+  let [글제목2,글제목변경]=useState('여자 코드 추천');
+
+  // 이 함수를 실행하면 array가 생성된다.
+  // ES6 destructuring 문법
+  // var [a,b]=[10,100];
+
   let posts="강남 고기 맛집";
-  
-  function abc(){
-    return 100
-  }
-  
+    
   return (
     <div className="App">
       <div className="black-nav">
@@ -20,7 +25,12 @@ function App() {
         <hr />
       </div>
       <div className="list">
-        <h3>{posts}</h3>
+        <h3>{글제목}</h3>
+        <p>2월 17일 발행</p>
+        <hr />
+      </div>
+      <div className="list">
+        <h3>{글제목2}</h3>
         <p>2월 17일 발행</p>
         <hr />
       </div>
