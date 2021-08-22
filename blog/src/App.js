d5a7ef8adcb5c2c25372fdac7cea56e7f5610c1c
kny <knyatom@gmail.com>
2021-08-23 01:21:30 +0900

    823
    823

--- blog/src/App.js
+++ blog/src/App.js
@@ -6,13 +6,14 @@ import logo from './logo.svg';
 function App() {
 
   let [글제목,글제목변경]=useState('남자 코드 추천');
-  let [글제목2,글제목변경]=useState('여자 코드 추천');
+  let [글제목2,글제목변경2]=useState('여자 코드 추천');
+  let [글제목3,글제목변경3]=useState(['남자 코드 추천','여자 코드 추천']);
 
   // 이 함수를 실행하면 array가 생성된다.
   // ES6 destructuring 문법
   // var [a,b]=[10,100];
 
-  let posts="강남 고기 맛집";
+  let posts="강남 고기 맛집2";
     
   return (
     <div className="App">
@@ -31,6 +32,16 @@ function App() {
       </div>
       <div className="list">
         <h3>{글제목2}</h3>
+        <p>2월 17일 발행</p>
+        <hr />
+      </div>
+      <div className="list">
+        <h3>{글제목3}</h3>
+        <p>2월 17일 발행</p>
+        <hr />
+      </div>
+      <div className="list">
+        <h3>{글제목3[1]}</h3>
         <p>2월 17일 발행</p>
         <hr />
       </div>
