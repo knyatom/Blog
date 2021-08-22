53eabf8ecbe2833f44b16cae3e19af9b1d61fc73
kny <knyatom@gmail.com>
2021-08-23 01:57:09 +0900

    888
    888

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,50 +1,40 @@
 
+/* eslint-disable */
+
 import React,{useState} from 'react';
 import './App.css';
 import logo from './logo.svg';
 
 function App() {
 
-  let [글제목,글제목변경]=useState('남자 코드 추천');
-  let [글제목2,글제목변경2]=useState('여자 코드 추천');
-  let [글제목3,글제목변경3]=useState(['남자 코드 추천','여자 코드 추천']);
+  let [글제목,글제목변경]=useState(['남자 코드 추천','강남 우동맛집','파이션독학']);  
+  let posts="강남 고기 맛집";
+  let [따봉,따봉변경]=useState(0);     
 
-  // 이 함수를 실행하면 array가 생성된다.
-  // ES6 destructuring 문법
-  // var [a,b]=[10,100];
-
-  let posts="강남 고기 맛집2";
-    
   return (
     <div className="App">
       <div className="black-nav">
-        <div className={posts}> 개발 Blog</div>
+        <div> 개발 Blog</div>
       </div>
+      <button onClick={ ()=>글제목변경(['여자 코드 추천','강남 우동맛집','리액트독학']) }>
+      제목 변경</button>
       <div className="list">
-        <h3>{posts}</h3>
+        <h3>{글제목[0]} 
+          <span onClick={ ()=>{따봉변경(따봉+1)} } >👍</span> {따봉} 
+         </h3>
         <p>2월 17일 발행</p>
         <hr />
       </div>
       <div className="list">
-        <h3>{글제목}</h3>
+        <h3>{글제목[1]}</h3>
         <p>2월 17일 발행</p>
         <hr />
       </div>
       <div className="list">
-        <h3>{글제목2}</h3>
+        <h3>{글제목[2]}</h3>
         <p>2월 17일 발행</p>
         <hr />
-      </div>
-      <div className="list">
-        <h3>{글제목3}</h3>
-        <p>2월 17일 발행</p>
-        <hr />
-      </div>
-      <div className="list">
-        <h3>{글제목3[1]}</h3>
-        <p>2월 17일 발행</p>
-        <hr />
-      </div>
+      </div>      
     </div>
   );  
 }
