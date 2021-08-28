d6f27d4955bbf72c1e2e41b63c0e046ef3bbdc55
knyatom <knyatom@daum.netm>
2021-08-28 23:13:07 +0900

    aa
    aa

--- blog/src/App.js
+++ blog/src/App.js
@@ -5,21 +5,38 @@ import "./App.css";
 import logo from "./logo.svg";
 
 function App() {
-  let [글제목, 글제목변경] = useState(["남자 코드 추천", "강남 우동맛집", "파이션독학"]);
+  let [글제목, 글제목변경] = useState([
+    "남자 코드 추천",
+    "강남 우동맛집",
+    "파이션독학",
+  ]);
   let posts = "강남 고기 맛집";
   let [따봉, 따봉변경] = useState(0);
   let [modal, modal변경] = useState(false);
+
+  function 반복된UI(){
+    var 어레이=[];
+    for(var i=0;i<3;i++){
+      어레이.push(<div>Hello</div>)
+    }
+    return 어레이
+  }
+  
 
   return (
     <div className="App">
       <div className="black-nav">
         <div> 개발 Blog</div>
       </div>
-      <button onClick={() => 글제목변경(["여자 코드 추천", "강남 우동맛집", "리액트독학"])}>
+      <button
+        onClick={() =>
+          글제목변경(["여자 코드 추천", "강남 우동맛집", "리액트독학"])
+        }
+      >
         제목 변경
       </button>
 
-      <div className="list">
+      {/* <div className="list">
         <h3>
           {글제목[0]}
           <span
@@ -36,31 +53,37 @@ function App() {
       </div>
 
       <div className="list">
-        <h3>
-          {글제목[1]}
-        </h3>
+        <h3>{글제목[1]}</h3>
         <p>2월 17일 발행</p>
         <hr />
       </div>
 
       <div className="list">
-        <h3>
-          {글제목[2]}
-        </h3>
+        <h3>{글제목[2]}</h3>
 
         <p>2월 17일 발행</p>
         <hr />
-      </div>
+      </div> */}
 
-      {글제목.map(글 =>
-        <div className="list">
-          <h3>
-            {글}
-          </h3>
-          <p>2월 17일 발행</p>
-          <hr />
-        </div>
-      )}
+      {글제목.map(function (글) {
+        return (
+          <div className="list">
+            <h3>
+              {글}{" "}
+              <span
+                onClick={() => {
+                  따봉변경(따봉 + 1);
+                }}
+              >
+                👍
+              </span>{" "}
+              {따봉}
+            </h3>
+            <p>2월 17일 발행</p>
+            <hr />
+          </div>
+        );
+      })}
 
       <button
         onClick={() => {
@@ -69,7 +92,9 @@ function App() {
       >
         모달
       </button>
-      {modal == true ? <Modal /> : null}
+      {modal == true ? <Modal></Modal> : null}
+
+      {반복된UI()}
     </div>
   );
 }
